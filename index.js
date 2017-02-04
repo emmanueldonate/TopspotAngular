cf15f477e095e1f3cd3cadbfecf1c0d893cff19f
Emmanuel Donate <aion@Emmanuels-MacBook-Pro.local>
2017-02-04 15:33:53 -0800

    new

---
+++ index.js
@@ -0,0 +1,10 @@
+var app = angular.module('app', []);
+
+app.controller('myCtrl', function($scope, $http){
+
+$http.get("topspots.json").success(function (response) {
+
+  $scope.topspots = response;
+
+});
+});
